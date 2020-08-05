20de0e48e283ca928cbb8146aaed1f488171a7be
孙帅旗 <sunshuaiqi@jsb5.com>
2020-08-05 17:04:13 +0800

    test

--- src/main/java/com/sx/ssqMVC/bean/test.java
+++ src/main/java/com/sx/ssqMVC/bean/test.java
@@ -4,4 +4,9 @@ package com.sx.ssqMVC.bean;
  * Created by sunsq on 2020/8/5.
  */
 public class test {
+
+    public static void main(String[] args) {
+        System.out.println("fenzhiyi");
+    }
+
 }
